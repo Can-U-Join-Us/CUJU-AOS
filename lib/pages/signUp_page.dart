3851e83fc3958bd0a5f5914a539d9c2bfc617ccd
k906506 <k906506@naver.com>
2021-11-16 17:31:17 +0900

    회원가입 Validator 추가

--- lib/pages/signUp_page.dart
+++ lib/pages/signUp_page.dart
@@ -11,6 +11,21 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   final formKey = GlobalKey<FormState>();
 
+  Future<void> _submit() async {
+    if (formKey.currentState!.validate() == false) {
+      return;
+    } else {
+      formKey.currentState!.save();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("가입이 완료되었습니다. 로그인을 진행해주세요."),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,13 +49,16 @@ class _SignUpPageState extends State<SignUpPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 TitleTextComponent("이름"),
-                TextFormFieldComponent(false, TextInputType.name, TextInputAction.next, "이름을 입력해주세요."),
+                TextFormFieldComponent(false, TextInputType.name,
+                    TextInputAction.next, "이름을 입력해주세요.", 3),
                 SizedBox(height: 20),
                 TitleTextComponent("휴대폰 번호"),
-                TextFormFieldComponent(false, TextInputType.number, TextInputAction.next, "(예시) 01012341234"),
+                TextFormFieldComponent(false, TextInputType.number,
+                    TextInputAction.next, "(예시) 01012341234", 11),
                 SizedBox(height: 20),
                 TitleTextComponent("계정 이메일"),
-                TextFormFieldComponent(false, TextInputType.emailAddress, TextInputAction.next, "계정 이메일을 입력해주세요."),
+                TextFormFieldComponent(false, TextInputType.emailAddress,
+                    TextInputAction.next, "계정 이메일을 입력해주세요.", 10),
                 SizedBox(height: 20),
                 TitleTextComponent("비밀번호"),
                 Container(
@@ -53,13 +71,14 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                   margin: EdgeInsets.only(bottom: 10),
                 ),
-                TextFormFieldComponent(true, TextInputType.visiblePassword, TextInputAction.done, "비밀번호를 입력해주세요."),
+                TextFormFieldComponent(true, TextInputType.visiblePassword,
+                    TextInputAction.done, "비밀번호를 입력해주세요.", 10),
                 SizedBox(height: 20),
                 TitleTextComponent("비밀번호 확인"),
-                TextFormFieldComponent(true, TextInputType.visiblePassword, TextInputAction.next, "비밀번호를 다시 한번 해주세요."),
+                TextFormFieldComponent(true, TextInputType.visiblePassword,
+                    TextInputAction.next, "비밀번호를 다시 한번 해주세요.", 10),
                 SizedBox(height: 20),
                 CheckBoxComponent("개인정보 수집 및 이용 동의 (필수)"),
-                CheckBoxComponent("이벤트 소식 등 알림 정보 받기"),
                 SizedBox(height: 20),
                 FlatButton(
                   child: Container(
@@ -78,13 +97,7 @@ class _SignUpPageState extends State<SignUpPage> {
                         textAlign: TextAlign.center),
                   ),
                   onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("가입이 완료되었습니다. 로그인을 진행해주세요."),
-                        duration: Duration(seconds: 1),
-                      ),
-                    );
-                    Navigator.of(context).pop();
+                    _submit();
                   },
                 ),
               ],
@@ -105,30 +118,34 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
-  Widget TextFormFieldComponent(bool obscureText, TextInputType keyboardType, TextInputAction textInputAction, String hintText)  {
+  Widget TextFormFieldComponent(bool obscureText, TextInputType keyboardType,
+      TextInputAction textInputAction, String hintText, int maxSize) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: Colors.grey,
-        ),
-      ),
       child: TextFormField(
         obscureText: obscureText,
         keyboardType: keyboardType,
         textInputAction: textInputAction,
         decoration: InputDecoration(
-          border: InputBorder.none,
-          focusedBorder: InputBorder.none,
           hintText: hintText,
         ),
+        validator: (value) {
+          if (value!.length < maxSize) {
+            return "형식에 맞게 입력해주세요!";
+          }
+        },
       ),
     );
   }
 
   Widget CheckBoxComponent(String keyword) {
     return CheckboxListTileFormField(
+      validator: (value) {
+        if (value == false) {
+          return "필수 동의 항목입니다.";
+        } else {
+          return null;
+        }
+      },
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
