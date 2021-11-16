c843798cc02735cca94be16215c474ea4d3be878
k906506 <k906506@naver.com>
2021-11-16 17:31:08 +0900

    로그인 Validator 추가

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:canyoujoinus/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   static const routeName = "/login";
@@ -9,6 +13,31 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
+  var _isLoading = false;
+
+  Future<void> _submit() async {
+    if (formKey.currentState!.validate() == false) {
+      return;
+    } else {
+      formKey.currentState!.save();
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<AuthProvider>(context, listen: false).login();
+      setState(() {
+        _isLoading = false;
+      });
+      if (Provider.of<AuthProvider>(context, listen: false).isAuth) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("로그인에 성공하였습니다."),
+            duration: Duration(seconds: 1),
+          ),
+        );
+        Navigator.of(context).pop();
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,64 +67,74 @@ class _LoginPageState extends State<LoginPage> {
                   child: Image.asset("images/initial_4.png"),
                 ),
                 Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  padding: const EdgeInsets.only(top : 10, bottom: 10, left: 20, right: 20),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     border: Border.all(
                       color: Colors.grey,
                     ),
                   ),
-                  child: TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.next,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "계정 이메일을 입력해주세요.",
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.grey,
-                    ),
-                  ),
-                  child: TextFormField(
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "비밀번호를 입력해주세요.",
-                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          hintText: "계정 이메일을 입력해주세요.",
+                        ),
+                        validator: (value) {
+                          if (value!.length < 10) {
+                            return "이메일 형식에 맞게 입력해주세요!";
+                          }
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        child: TextFormField(
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          textInputAction: TextInputAction.done,
+                          decoration: InputDecoration(
+                            hintText: "비밀번호를 입력해주세요.",
+                          ),
+                          validator: (value) {
+                            if (value!.length < 10) {
+                              return "비밀번호는 10자 이상입니다.";
+                            }
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 SizedBox(height: 20),
-                FlatButton(
-                  child: Container(
-                    height: 40,
-                    width: double.infinity,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
-                    ),
-                    child: Text("로그인",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                _isLoading
+                    ? Container(
+                        alignment: Alignment.center,
+                        height: 40,
+                        child: CircularProgressIndicator(),
+                      )
+                    : FlatButton(
+                        child: Container(
+                          height: 40,
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey,
+                          ),
+                          child: Text("로그인",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                              textAlign: TextAlign.center),
                         ),
-                        textAlign: TextAlign.center),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
+                        onPressed: () {
+                          _submit();
+                        },
+                      ),
                 FlatButton(
                   child: Container(
                     alignment: Alignment.center,
